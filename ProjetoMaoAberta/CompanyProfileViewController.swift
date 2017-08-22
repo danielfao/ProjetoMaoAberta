4d982340c020b3b5dad2fdc640518bb162d4a003
Daniel Oliveira <daniel.fao@gmail.com>
2017-08-22 12:40:16 -0300

    Creates company profile screen

---
+++ ProjetoMaoAberta/CompanyProfileViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CompanyProfileViewController.swift
+//  ProjetoMaoAberta
+//
+//  Created by Daniel Oliveira on 8/22/17.
+//  Copyright © 2017 Daniel Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class CompanyProfileViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+
+    
+    //MARK: - Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - IBActions
+}
